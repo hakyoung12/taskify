176c89a48011cc5ecd31b8bf8190c7ddd35e380c
Homin <coc1381@naver.com>
2024-05-30 22:50:46 +0900

    feat: 초대받은 대시보드 컴포넌트 작성

--- app/mydashboard/page.tsx
+++ app/mydashboard/page.tsx
@@ -1,7 +1,15 @@
 import DashboardHeader from '@/components/DashboardHeader';
+import InvitedDashboardList from '@/components/InvitedDashboarList';
+import InvitedDashboardListMobile from '@/components/InvitedDashboardListMobile';
 
 const MyDashboard = () => {
-  return <DashboardHeader />;
+  return (
+    <>
+      <DashboardHeader />
+      <InvitedDashboardList />
+      <InvitedDashboardListMobile />
+    </>
+  );
 };
 
 export default MyDashboard;
