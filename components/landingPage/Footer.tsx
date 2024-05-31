1a8135c051871a2e6bb63cbd259ae324bfc1f368
waterkail <41028065+waterkail@users.noreply.github.com>
2024-05-31 18:43:01 +0900

    fix: "" =>

--- components/landingPage/Footer.tsx
+++ components/landingPage/Footer.tsx
@@ -5,48 +5,48 @@ export default function Footer() {
   const YEAR = new Date().getFullYear();
 
   return (
-    <footer className="flex justify-center items-center h-[100px] mt-[160px] text-gray-_9fa6b2 max-sm:text-xs max-sm:h-[216px] max-sm:items-start max-sm:mt-[120px]">
-      <div className="flex justify-between items-center px-10 max-w-[1720px] w-[100%] max-sm:flex-col">
-        <div className="max-sm:mb-3">@codeit-{YEAR}</div>
-        <div className="flex gap-8 max-sm:gap-5 max-sm:mb-[68px]">
-          <Link href="https://www.naver.com/">Privacy Policy</Link>
-          <Link href="https://www.naver.com/">FAQ</Link>
+    <footer className='flex justify-center items-center h-[100px] mt-[160px] text-gray-_9fa6b2 max-sm:text-xs max-sm:h-[216px] max-sm:items-start max-sm:mt-[120px]'>
+      <div className='flex justify-between items-center px-10 max-w-[1720px] w-[100%] max-sm:flex-col'>
+        <div className='max-sm:mb-3'>@codeit-{YEAR}</div>
+        <div className='flex gap-8 max-sm:gap-5 max-sm:mb-[68px]'>
+          <Link href='https://www.naver.com/'>Privacy Policy</Link>
+          <Link href='https://www.naver.com/'>FAQ</Link>
         </div>
-        <div className="flex items-center gap-3.5 max-sm:gap-5">
+        <div className='flex items-center gap-3.5 max-sm:gap-5'>
           <Link
-            href="https://www.naver.com/"
-            className="w-[20px] max-sm:w-[16px]"
+            href='https://www.naver.com/'
+            className='w-[20px] max-sm:w-[16px]'
           >
             <Image
-              src="/LandingPage/footerEmail.svg"
+              src='/LandingPage/footerEmail.svg'
               width={20}
               height={20}
-              alt="이메일"
-              layout="responsive"
+              alt='이메일'
+              layout='responsive'
             />
           </Link>
           <Link
-            href="https://www.facebook.com/"
-            className="w-[22px] max-sm:w-[18px]"
+            href='https://www.facebook.com/'
+            className='w-[22px] max-sm:w-[18px]'
           >
             <Image
-              src="/LandingPage/footerFacebook.svg"
+              src='/LandingPage/footerFacebook.svg'
               width={22}
               height={22}
-              alt="페이스북"
-              layout="responsive"
+              alt='페이스북'
+              layout='responsive'
             />
           </Link>
           <Link
-            href="https://www.instagram.com/"
-            className="w-[22px] max-sm:w-[18px]"
+            href='https://www.instagram.com/'
+            className='w-[22px] max-sm:w-[18px]'
           >
             <Image
-              src="/LandingPage/footerInstagram.svg"
+              src='/LandingPage/footerInstagram.svg'
               width={22}
               height={22}
-              alt="인스타그램"
-              layout="responsive"
+              alt='인스타그램'
+              layout='responsive'
             />
           </Link>
         </div>
