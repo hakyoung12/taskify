41d0b8d9c7a4840a08e32c07702a4082c51b9a47
Homin <coc1381@naver.com>
2024-05-31 21:22:38 +0900

    feat: 새로운 대시보드 추가 모달 생성

--- components/InvitedDashboardListMobile.tsx
+++ components/InvitedDashboardListMobile.tsx
@@ -7,12 +7,12 @@ const InvitedDashboardListMobile = () => {
 
   return (
     <div className='md:hidden ml-[60px] md:ml-[160px] px-7 py-8 border border-black'>
-      <div className='text-black-_333236 text-2xl font-bold'>
+      <div className='text-custom_black-_333236 text-2xl font-bold'>
         초대받은 대시보드
       </div>
       <div className='relative mt-5'>
         <input
-          className='w-full p-3 border border-gray-_d9d9d9 rounded-md indent-8 text-[16px]'
+          className='w-full p-3 border border-custom_gray-_d9d9d9 rounded-md indent-8 text-[16px]'
           placeholder='검색'
         />
         <img
@@ -25,18 +25,18 @@ const InvitedDashboardListMobile = () => {
         {data.map((item) => (
           <div key={item.id} className='mt-5'>
             <div className='flex gap-x-7 text-[14px]'>
-              <div className='text-gray-_9fa6b2'>이름</div>
+              <div className='text-custom_gray-_9fa6b2'>이름</div>
               <div>{item.name}</div>
             </div>
             <div className='flex gap-x-4 text-[14px]'>
-              <div className='text-gray-_9fa6b2'>초대자</div>
+              <div className='text-custom_gray-_9fa6b2'>초대자</div>
               <div className=''>{item.inviter}</div>
             </div>
             <div className='flex gap-x-[10px] mt-5'>
-              <button className='flex-1 px-7 py-2 bg-violet-_5534da text-white rounded'>
+              <button className='flex-1 px-7 py-2 bg-custom_violet-_5534da text-white rounded'>
                 수락
               </button>
-              <button className='flex-1 px-7 py-2 bg-white text-violet-_5534da rounded border border-gray-_d9d9d9'>
+              <button className='flex-1 px-7 py-2 bg-white text-custom_violet-_5534da rounded border border-custom_gray-_d9d9d9'>
                 거절
               </button>
             </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,44 +1,107 @@
 import type { Config } from 'tailwindcss';
 
-const config: Config = {
+const config = {
+  darkMode: ['class'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       colors: {
-        black: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        custom_black: {
           _000000: '#000000',
           _171717: '#171717',
           _333236: '#333236',
           _4b4b4b: '#4b4b4b',
         },
-        gray: {
+        custom_gray: {
           _787486: '#787486',
           _9fa6b2: '#9fa6b2',
           _d9d9d9: '#d9d9d9',
           _eeeeee: '#eeeeee',
           _fafafa: '#fafafa',
         },
-        violet: {
+        custom_violet: {
           _5534da: '#5534da',
           _8: '#f1effd',
         },
-        white: '#ffffff',
-        red: '#d6173a',
-        green: {
+        custom_white: '#ffffff',
+        custom_red: '#d6173a',
+        custom_green: {
           _7ac555: '#7ac555',
           _a3c4a2: '#a3c4a2',
         },
-        purple: '#760dde',
-        orange: '#ffa500',
-        blue: '#76a6ea',
-        pink: '#e876ea',
+        custom_purple: '#760dde',
+        custom_orange: '#ffa500',
+        custom_blue: '#76a6ea',
+        custom_pink: '#e876ea',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
+
 export default config;
