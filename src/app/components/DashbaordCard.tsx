e6570b1802c42aa06581ff4eb1ff5769c50d310a
hakyoung12 <gkr097@naver.com>
2024-06-01 18:32:59 +0900

    Style: 대시보드 카드리스트 스타일링 수정

--- src/app/components/DashbaordCard.tsx
+++ src/app/components/DashbaordCard.tsx
@@ -43,9 +43,9 @@ const mockData: Todo[] = [
 
 export default function DashboardCard() {
   return (
-    <section className='m-6 w-sreen'>
-      <div className='grid xl:grid-cols-3 xl:grid-rows-2 gap-3 h-[152px] sm:grid-cols-2 sm:grid-rows-3 sm:h-56 max-sm:grid-cols-1 max-sm:grid-rows-6 max-sm:h-96'>
-        <button className='flex items-center text-gray-800 justify-center gap-3 w-full h-16 font-Pretendard font-semibold px-5 py-7 text-base rounded-lg border border-gray-300 bg-white max-lg:w-56 max-sm:h-14'>
+    <section className='mt-6 ml-6 w-sreen'>
+      <div className='grid xl:grid-cols-3 xl:grid-rows-2 gap-3 sm:grid-cols-2 sm:grid-rows-3 max-sm:grid-cols-1 max-sm:grid-rows-6 max-sm:h-96'>
+        <button className='flex items-center text-gray-800 justify-center gap-3 w-full h-16 font-Pretendard font-semibold px-5 py-7 text-base rounded-lg border border-gray-300 bg-white max-sm:h-14'>
           새로운 대시보드
           <img
             className='w-5 h-5'
@@ -57,7 +57,7 @@ export default function DashboardCard() {
           <Link
             key={todo.id}
             href={`/dashboard/${todo.id}`}
-            className='flex items-center text-gray-800 justify-between w-full h-16 font-Pretendard font-semibold px-5 py-7 text-base rounded-lg border border-gray-300 bg-white max-lg:w-56 max-sm:h-14'
+            className='flex items-center text-gray-800 justify-between w-full h-16 font-Pretendard font-semibold px-5 py-7 text-base rounded-lg border border-gray-300 bg-white max-sm:h-14'
           >
             <div className='flex items-center gap-2'>
               <div className='flex items-center gap-4'>
