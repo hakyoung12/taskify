2df023111810f8975b999f627e09e71481e0b7a8
hakyoung12 <gkr097@naver.com>
2024-06-01 18:37:54 +0900

    feat: 초대내역 초대하기 버튼 추가

--- src/app/components/InvitationList.tsx
+++ src/app/components/InvitationList.tsx
@@ -4,8 +4,16 @@ import EditMenuTitle from './EditMenuTitle';
 export default function InvitationList() {
   return (
     <div className='w-auto rounded-lg bg-white-_ffffff m-5'>
-      <div>
+      <div className='relative flex'>
         <EditMenuTitle title='초대 내역' subtitle='이메일' />
+        <button className='flex items-center gap-2 mt-8 py-2 px-3 w-[105px] h-8 text-white rounded-md bg-custom_violet-_5534da font-Pretendard font-medium text-base max-sm:absolute max-sm:bottom-4 max-sm:right-7 max-sm:text-sx py-2 px-3'>
+          <img
+            src='/images/inviteMemberButton.svg'
+            className='w-4 h-4 max-sm:w-3.5 max-sm:h-3.5'
+            alt='초대하기 버튼'
+          />
+          초대하기
+        </button>
       </div>
       <div className='flex flex-col px-[28px] pb-[28px]'>
         <div className='flex justify-between items-center flex-shrink-0 py-4 text-black-_333236 border-b stroke-gray-_eeeeee stroke-1'>
