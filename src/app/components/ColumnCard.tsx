18723b12317357009747ddd0cd929f975628167c
김보민 <bomin@Bomin-MacBook.local>
2024-06-01 18:59:52 +0900

    Comment: 주석 추가

--- src/app/components/ColumnCard.tsx
+++ src/app/components/ColumnCard.tsx
@@ -20,6 +20,7 @@ export default function ColumnCard({
   return (
     <>
       <div className='bg-white rounded-md border border-gray-_d9d9d9 flex flex-col justify-center p-[20px] gap-[10px]'>
+        {/* 카드 이미지 설정 */}
         {imageUrl && (
           <Image
             width={274}
@@ -29,19 +30,20 @@ export default function ColumnCard({
             alt=''
           />
         )}
+        {/* 제목 */}
         <span>{title}</span>
-        <div className='flex gap-[6px]'>
-          {tags.map((tag: any, index: number) => {
-            return (
-              <div
-                className='bg-[#F9EEE3] rounded text-[#D58D49] text-[12px] px-[6px] py-[4px]'
-                key={index}
-              >
-                {tag}
-              </div>
-            );
-          })}
-        </div>
+        {/* 태그 */}
+        {tags.map((tag: any, index: number) => {
+          return (
+            <div
+              className='bg-[#F9EEE3] rounded text-[#D58D49] text-[12px] px-[6px] py-[4px] mr-[6px]'
+              key={index}
+            >
+              {tag}
+            </div>
+          );
+        })}
+        {/* 카드 마감날짜와 관리자 아이콘 */}
         <div className='flex text-gray-_787486 text-[12px] relative flex justify-between justify-center items-center'>
           <div className='flex gap-[6px]'>
             <img src='/images/calender-icon.svg' alt='캘린더 아이콘' />
