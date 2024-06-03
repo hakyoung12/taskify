03de67d45a97ef3b4046bad435ec16f91a1d33e3
hakyoung12 <gkr097@naver.com>
2024-06-03 16:30:35 +0900

    refactor: mydashboard 페이지 헤더 변경

--- src/app/mydashboard/page.tsx
+++ src/app/mydashboard/page.tsx
@@ -3,7 +3,6 @@ import InvitedDashboardList from '../components/InvitedDashboarList';
 import InvitedDashboardListMobile from '../components/InvitedDashboardListMobile';
 import SideBar from '../components/SideBar';
 import DashboardCard from '../components/DashbaordCard';
-import InvitationList from '../components/InvitationList';
 import MemberList from '../components/MemberList';
 import UpdateDashboardName from '../components/UpdateDashboardName';
 import DashboardHeaderInSettings from '../components/DashboardHeaderInSettings';
@@ -11,15 +10,15 @@ import DashboardHeaderInSettings from '../components/DashboardHeaderInSettings';
 const MyDashboard = () => {
   return (
     <>
-      <DashboardHeaderInSettings />
       <div className='flex'>
         <SideBar />
         <div className='w-3/4'>
-          <DashboardHeader />
+          <DashboardHeaderInSettings />
           <DashboardCard />
           <InvitedDashboardList />
           <InvitedDashboardListMobile />
           <UpdateDashboardName />
+          <MemberList />
         </div>
       </div>
     </>
