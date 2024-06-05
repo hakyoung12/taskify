3e0e9b6731961f4b253ece01799ff8fabdd8d750
Homin <coc1381@naver.com>
2024-06-05 20:48:08 +0900

    feat: header 에 dropdown 메뉴 추가 및 로그아웃 기능 추가

--- src/app/components/DashboardHeader.tsx
+++ src/app/components/DashboardHeader.tsx
@@ -4,9 +4,24 @@ import { useEffect, useState } from 'react';
 import { LOGIN_TOKEN } from '../api/apiStrings';
 import { CheckUserRes } from '../api/apiTypes/userType';
 import instance from '../api/axios';
+import { useRouter } from 'next/navigation';
 
 const DashboardHeader = () => {
   const [user, setUser] = useState<CheckUserRes | null>(null);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const router = useRouter();
+
+  const handleNicknameClick = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem(LOGIN_TOKEN);
+    setUser(null);
+    setShowDropdown(false);
+    router.push('/');
+  };
 
   useEffect(() => {
     const accessToken = localStorage.getItem(LOGIN_TOKEN);
@@ -19,7 +34,7 @@ const DashboardHeader = () => {
       }
     };
 
-    accessToken ? fetchUserData() : setUser(null);
+    accessToken ? fetchUserData() : router.push('/');
   }, []);
 
   return (
@@ -27,15 +42,32 @@ const DashboardHeader = () => {
       <div className='ml-10 text-lg font-bold text-custom_black-_333236'>
         내 대시보드
       </div>
-      <div className='flex items-center'>
-        <div className='relative mx-3 h-[34px] w-[34px] rounded-full border-2 border-white bg-blue-500 text-white'>
-          <p className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform'>
-            {user && user.nickname[0]}
-          </p>
+      <div className='relative'>
+        <div className='flex items-center' onClick={handleNicknameClick}>
+          <div className='relative mx-3 h-[34px] w-[34px] cursor-pointer rounded-full border-2 border-white bg-blue-500 text-white'>
+            <p className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform'>
+              {user && user.nickname[0]}
+            </p>
+          </div>
+          <div className='mr-[80px] hidden w-[45px] cursor-pointer sm:block'>
+            {user && user.nickname}
+          </div>
         </div>
-        <div className='mr-[80px] hidden w-[45px] sm:block'>
-          {user && user.nickname}
-        </div>
+        {showDropdown && (
+          <div className='absolute right-0 top-full z-10 mt-1 rounded border border-gray-300 bg-white shadow-md'>
+            <ul>
+              <li
+                onClick={handleLogout}
+                className='cursor-pointer px-4 py-2 hover:bg-gray-100'
+              >
+                로그아웃
+              </li>
+              <li className='cursor-pointer px-4 py-2 hover:bg-gray-100'>
+                마이페이지
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
