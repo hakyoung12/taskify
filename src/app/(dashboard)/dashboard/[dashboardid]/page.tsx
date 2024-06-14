0eb84d3659d316200d7f9f5c76dc1c8fff3e4508
김보민 <bomin@Bomin-MacBook.local>
2024-06-15 00:00:34 +0900

    Fix: newcolumn버튼 스타일 수정

--- src/app/(dashboard)/dashboard/[dashboardid]/page.tsx
+++ src/app/(dashboard)/dashboard/[dashboardid]/page.tsx
@@ -61,22 +61,24 @@ export default function dashboardPage(dashboardid: any) {
                 />
               );
             })}
-          <div
-            className='border-gray-_d9d9d9 ml-[20px] mt-[68px] flex h-[70px] min-w-[354px] items-center justify-center rounded-lg border bg-white max-xl:ml-[0px] max-xl:mt-[0px]'
-            onClick={() =>
-              handleOpenModal(
-                <NewColumnModal
-                  dashboardId={dashboardID}
-                  columnTitles={columnTitles}
-                  setIsColumnChange={setIsColumnChange}
-                />,
-              )
-            }
-          >
-            <p className='mr-[12px] text-[16px] font-bold'>
-              새로운 컬럼 추가하기
-            </p>
-            <ChipAddIcon size={'large'} />
+          <div className='px-[20px] pt-[68px] max-xl:p-[10px]'>
+            <div
+              className='border-gray-_d9d9d9flex flex h-[70px] min-w-[354px] items-center justify-center rounded-lg border bg-white'
+              onClick={() =>
+                handleOpenModal(
+                  <NewColumnModal
+                    dashboardId={dashboardID}
+                    columnTitles={columnTitles}
+                    setIsColumnChange={setIsColumnChange}
+                  />,
+                )
+              }
+            >
+              <p className='mr-[12px] text-[16px] font-bold'>
+                새로운 컬럼 추가하기
+              </p>
+              <ChipAddIcon size={'large'} />
+            </div>
           </div>
         </div>
       </div>
